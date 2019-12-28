00fc81d845a78eeb1d2d545eb4e2bc4b064446a3
vadim_dorofeev <vadim_dorofeev@relex.ru>
2019-12-28 15:13:10 +0300

    старт работ по Bugify

---
+++ rest-sample/src/test/java/ru/stqa/pft/rest/RestTest.java
@@ -0,0 +1,36 @@
+package ru.stqa.pft.rest;
+
+import org.apache.http.client.fluent.Executor;
+import org.apache.http.client.fluent.Request;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.Set;
+
+import static org.testng.Assert.assertEquals;
+
+public class RestTest {
+
+    @Test
+    public void testCreateIssue() throws IOException {
+        Set<Issue> oldIssues = getIssues();
+        Issue newIssue = new Issue();
+        int issueId = createIssue(newIssue);
+        Set<Issue> newIssues = getIssues();
+        oldIssues.add(newIssue.withId(issueId));
+        assertEquals(newIssues, oldIssues);
+    }
+
+    private int createIssue(Issue newIssue) {
+        return 0;
+    }
+
+    private Set<Issue> getIssues() throws IOException {
+        String json = getExecutor().execute(Request.Get("http://demo.bugify.com/api/issues.json")).returnContent().asString();
+        return null;
+    }
+
+    private Executor getExecutor(){
+        return Executor.newInstance().auth("288f44776e7bec4bf44fdfeb1e646490","");
+    }
+}
